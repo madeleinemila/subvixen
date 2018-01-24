f893fddd32d2e0643a04ad72ebfb78ba996562df
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2018-01-24 14:54:33 +1100

    added video thumbnails and nav

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -37,6 +37,13 @@ class VideosController < ApplicationController
 
   def index
     @videos = Video.all
+    @thumbs = []
+    @videos.each do |v|
+      segs = /(https:\/\/res.cloudinary.com\/mrmy\/video\/upload\/)(.*\.)/.match( v.url )
+      thumb = segs[1] + "so_0,w_150,h_150,c_fill/" + segs[2] + "jpg"
+      @thumbs.push thumb
+    end
+    @thumbs
   end
 
   def show
