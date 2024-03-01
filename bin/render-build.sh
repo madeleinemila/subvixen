3b8261d1bb4bf871da596f9e14827e44e2999503
madeleinemila <32352098+madeleinemila@users.noreply.github.com>
2024-03-02 00:18:33 +0100

    Add db migrate to build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,3 +5,7 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+bundle exec rails db:migrate
+
+# if you have seeds to run add:
+bundle exec rails db:seed
